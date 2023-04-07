5bd58e8d51f5afb71fa530e7b4a4107264db9aa9
Kiara Jeff <ki.mij@icloud.com>
2023-04-07 05:46:05 +0000

    added scissors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  layout(false)
+  layout("wrapper.html.erb")
 
   # Add your actions below this line
   # ================================
@@ -30,5 +30,19 @@ class ApplicationController < ActionController::Base
     end
 
     render({ :template => "game_templates/user_paper.html.erb" })
+  end
+
+  def play_scissors
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "lost"
+    elsif @comp_move == "scissors"
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "won"
+    end
+
+    render({ :template => "game_templates/user_scissors.html.erb" })
   end
 end
